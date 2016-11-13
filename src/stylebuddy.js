33cb22a8dbc8d7a02920f93e3cf28162b5805e12
David Neubauer <davidneub@gmail.com>
2016-11-13 16:24:54 +0100

    Add pseudo selectors and at rules support

---
+++ src/stylebuddy.js
@@ -0,0 +1,74 @@
+const AT_RULE_NESTED = 'At-rule nested in pseudo selector';
+
+const createProperty = (property, value) => `${property}:${value};`;
+const createRuleSet = (selector, block) => (block !== '' ? `${selector}{${block}}` : '');
+const convertCamelCase = input => input.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+const isPropertyPseudoSelector = property => property[0] === ':';
+const isAtRule = property => property[0] === '@';
+
+const renderStyleProperties = (selector, atRuleNotAllowed) => (
+  Object.keys(selector).map(property => {
+    if (atRuleNotAllowed && isAtRule(property)) {
+      throw new Error(AT_RULE_NESTED);
+    }
+    return createProperty(
+      convertCamelCase(property),
+      selector[property]
+    );
+  })
+);
+
+const renderPseudoSelectors = (pseudoSelectors, element) => (
+  Object.keys(pseudoSelectors).map(selector => createRuleSet(
+    element + selector,
+    renderStyleProperties(pseudoSelectors[selector], true)
+  ))
+);
+
+const renderAtRules = (atRules, selector) => (
+  Object.keys(atRules).map(rule => createRuleSet(
+    rule,
+    render({ [selector]: atRules[rule] })
+  ))
+);
+
+const prepareBlock = block => {
+  const pseudoSelectors = {};
+  const atRules = {};
+  const styleProperties = {};
+
+  Object.keys(block).forEach(property => {
+    if (isPropertyPseudoSelector(property)) {
+      pseudoSelectors[property] = block[property];
+    } else if (isAtRule(property)) {
+      atRules[property] = block[property];
+    } else {
+      styleProperties[property] = block[property];
+    }
+  });
+
+  return {
+    styleProperties,
+    pseudoSelectors,
+    atRules,
+  };
+};
+
+const render = obj => (
+  Object.keys(obj).map(selector => {
+    const { styleProperties, pseudoSelectors, atRules } = prepareBlock(obj[selector]);
+    return (
+      createRuleSet(selector, renderStyleProperties(styleProperties).join('')) +
+      renderPseudoSelectors(pseudoSelectors, selector).join('') +
+      renderAtRules(atRules, selector)
+    );
+  }).join('')
+);
+
+const create = (style) => ({
+  render: () => render(style),
+});
+
+module.exports = {
+  create,
+};
---
+++ src/stylebuddy.test.js
@@ -0,0 +1,103 @@
+const assert = require('assert');
+const stylebuddy = require('./api');
+
+test('api.render returns the parsed css', () => {
+  const input = {
+    body: {
+      background: 'black',
+    },
+  };
+
+  assert.equal(stylebuddy.create(input).render(), 'body{background:black;}');
+});
+
+test('api.render supports pseudo selectors', () => {
+  const input = {
+    button: {
+      background: 'yellow',
+      ':hover': {
+        background: 'blue',
+      },
+    },
+  };
+
+  assert.equal(
+    stylebuddy.create(input).render(),
+    'button{background:yellow;}button:hover{background:blue;}'
+  );
+});
+
+test('api.render converts camel case properties into dash hyphen', () => {
+  const input = {
+    button: {
+      background: '#fff',
+      borderColor: 'black',
+    },
+  };
+
+  assert.equal(stylebuddy.create(input).render(), 'button{background:#fff;border-color:black;}');
+});
+
+test('api.render supports media queries', () => {
+  const input = {
+    body: {
+      '@media screen and (min-width:720px)': {
+        background: 'black',
+      },
+    },
+  };
+
+  assert.equal(
+    stylebuddy.create(input).render(),
+    '@media screen and (min-width:720px){body{background:black;}}'
+  );
+});
+
+test('api.render throws when media queries are nested within a pseudo selector', () => {
+  const input = {
+    body: {
+      ':hover': {
+        '@media screen and (min-width:720px)': {
+          background: 'black',
+        },
+      },
+    },
+  };
+
+  assert.throws(() => stylebuddy.create(input).render(), /At-rule nested in pseudo selector/);
+});
+
+test('api.render supports media queries containing a pseudo selector', () => {
+  const input = {
+    body: {
+      '@media screen and (min-width:720px)': {
+        ':hover': {
+          background: 'black',
+        },
+      },
+    },
+  };
+
+  assert.equal(
+    stylebuddy.create(input).render(),
+    '@media screen and (min-width:720px){body:hover{background:black;}}'
+  );
+});
+
+test('api.render supports nested media queries', () => {
+  const input = {
+    body: {
+      '@media screen': {
+        background: 'black',
+        '@media (min-width:700px)': {
+          background: 'red',
+        },
+      },
+    },
+  };
+
+  assert.equal(
+    stylebuddy.create(input).render(),
+    '@media screen{body{background:black;}@media (min-width:700px){body{background:red;}}}'
+  );
+});
